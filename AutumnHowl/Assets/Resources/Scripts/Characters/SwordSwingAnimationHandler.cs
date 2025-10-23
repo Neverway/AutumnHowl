09f1637f06cadf32421fb51e06c30348571c65e3
errynei <its.errynei@gmail.com>
2025-10-23 05:40:50 -0700

    Getting them animations working HUZZAH

--- AutumnHowl/Assets/Resources/Scripts/Characters/SwordSwingAnimationHandler.cs
+++ AutumnHowl/Assets/Resources/Scripts/Characters/SwordSwingAnimationHandler.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class SwordSwingAnimationHandler : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private string[] pullbackDirectionStates;
+    [SerializeField] private string spinState;
+
+    [Space]
     [SerializeField] private Transform visualContainer_default;
     [SerializeField] private Transform visualContainer_swingSword;
 
+    [Space]
+    [Header("Input controls")]
+    public SwingState swingState;
+    public enum SwingState { None, Pullback, Spin, Recoil }
+    [Space]
     public Direction attackStartDirection;
     public SpinDirection spinDireciton;
-    public SwingState swingState;
-
-    public enum SwingState { None, Pullback, Spin, Recoil }
-
-    [Range(0f, 1f)]
-    public float swordPullbackFactor = 0f;
+    [Range(0f, 1f)] public float swordPullbackFactor = 0f;
+    public float spinDegreesRotation;
 
     public void Update()
     {
@@ -29,10 +36,47 @@ public class SwordSwingAnimationHandler : MonoBehaviour
 
     public void OnPullback()
     {
-
+        Direction direcitonToUse = attackStartDirection;
+        switch (spinDireciton)
+        {
+            case SpinDirection.Left:
+                visualContainer_swingSword.localScale = new Vector3(1f, 1f, 1f);
+                break;
+            case SpinDirection.Right:
+                visualContainer_swingSword.localScale = new Vector3(-1f, 1f, 1f);
+                switch (attackStartDirection)
+                {
+                    case Direction.East: direcitonToUse = Direction.West; break;
+                    case Direction.West: direcitonToUse = Direction.East; break;
+                }
+                break;
+            default:
+                return;
+        }
+        string pullbackState = pullbackDirectionStates[(int)direcitonToUse];
+        
+        float factor = Mathf.Min(swordPullbackFactor, 0.99f);
+        animator.Play(pullbackState, 0, factor);
+        animator.speed = 0f;
     }
     public void OnSpin()
     {
+        float degrees = spinDegreesRotation;
+        switch (spinDireciton)
+        {
+            case SpinDirection.Left:
+                visualContainer_swingSword.localScale = new Vector3(1f, 1f, 1f);
+                break;
+            case SpinDirection.Right:
+                visualContainer_swingSword.localScale = new Vector3(-1f, 1f, 1f);
+                degrees = -degrees;
+                break;
+            default:
+                return;
+        }
+        float factor = (degrees / 360f) % 1f;
+        animator.Play(spinState, 0, factor);
+        animator.speed = 0f;
 
     }
     public void OnRecoil()
