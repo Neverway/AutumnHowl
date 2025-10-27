44d9511d8f4519034140c338dfdf8117217d7ae6
errynei <its.errynei@gmail.com>
2025-10-26 23:33:08 -0700

    Added tags to character templates

--- AutumnHowl/Assets/Resources/Scripts/GameInstance/GI_CharacterReferencer.cs
+++ AutumnHowl/Assets/Resources/Scripts/GameInstance/GI_CharacterReferencer.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class GI_CharacterReferencer : MonoBehaviour
 {
     private List<Character> activeCharacterComponents = new List<Character>();
-    private List<CharacterIdentifier> activeCharacterIdentifiers = new List<CharacterIdentifier>();
 
     public void Register(Character character) => activeCharacterComponents.Add(character);
     public void UnRegister(Character character) => activeCharacterComponents.Remove(character);
-    public void Register(CharacterIdentifier identifier) => activeCharacterIdentifiers.Add(identifier);
-    public void UnRegister(CharacterIdentifier identifier) => activeCharacterIdentifiers.Remove(identifier);
 }
 
