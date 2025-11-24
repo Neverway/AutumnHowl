4297227758eee1ce92b6e50cefab3250610a543b
lizband <lizband.dev@gmail.com>
2025-11-24 18:26:14 -0500

    Added skipping to intro cutscene

---
+++ AutumnHowl/Assets/Resources/Scripts/LB_Intro.cs
@@ -0,0 +1,55 @@
+//==========================================( Neverway 2025 )=========================================================//
+// Author
+//  Liz M.
+//
+// Contributors
+//
+//
+//====================================================================================================================//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LB_Intro : MonoBehaviour
+{
+    #region========================================( Variables )======================================================//
+    /*-----[ Inspector Variables ]------------------------------------------------------------------------------------*/
+
+
+    /*-----[ External Variables ]-------------------------------------------------------------------------------------*/
+
+
+    /*-----[ Internal Variables ]-------------------------------------------------------------------------------------*/
+
+
+    /*-----[ Reference Variables ]------------------------------------------------------------------------------------*/
+
+
+    #endregion
+
+
+    #region=======================================( Functions )=======================================================//
+    /*-----[ Mono Functions ]-----------------------------------------------------------------------------------------*/
+
+
+    /*-----[ Internal Functions ]-------------------------------------------------------------------------------------*/
+    public void Update()
+    {
+        if (GameInstance.Inputs.Interact.WasPressedThisFrame() || 
+            GameInstance.Inputs.Action.WasPressedThisFrame() ||
+            GameInstance.Inputs.Select.WasReleasedThisFrame() || 
+            GameInstance.Inputs.Start.WasReleasedThisFrame())
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+
+
+    /*-----[ External Functions ]-------------------------------------------------------------------------------------*/
+
+
+    #endregion
+}
